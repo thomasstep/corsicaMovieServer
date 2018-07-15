d6942540f6b8847aaf48431a6b49aeebb93d935c
Thomas <thomasstep@outlook.com>
2018-07-14 20:44:52 -0700

    Got rid of Material UI problems. Working on cards.

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/movie/:id", (req, res, next) => {
 app.get("/download/:path", (req, res, next) => {
   var movieDownloadPath = req.params.path;
   // Need to figure out how to copy to USB from here
-  fs.createReadStream(__dirname + movieDownloadPath).pipe(fs.createWriteStream("needsToBeUsb.mp4"));
+  fs.createReadStream(__dirname + "/movie/" + movieDownloadPath).pipe(fs.createWriteStream("needsToBeUsb.mp4"));
   res.status(200).send("It worked");
 });
 
